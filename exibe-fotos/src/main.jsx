fd5cf16d71835f9fe9540a921dbd72ac9f401a30
Leonardo Moura <leohdmoura@gmail.com>
2025-05-10 12:55:55 -0300

    learn(07-react): encerra a apostila
    -retomar fazendo o back end entre front e API Pexels a fim de isolar chave

--- exibe-fotos/src/main.jsx
+++ exibe-fotos/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.min.css'
-import 'primereact/resources/themes/arya-orange/theme.css'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
